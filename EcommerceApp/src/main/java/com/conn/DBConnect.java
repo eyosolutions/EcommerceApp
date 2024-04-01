b8d3a3cb4df8813b59d20bda79a4b11850b73026
Chriscloudaz <chriscloudaz@gmail.com>
2024-04-01 11:50:28 +0100

    updated DB url

--- EcommerceApp/src/main/java/com/conn/DBConnect.java
+++ EcommerceApp/src/main/java/com/conn/DBConnect.java
@@ -11,15 +11,11 @@ public class DBConnect
 	public static Connection getConn()
 	{
 		try {
-			String url = System.getenv("DB_URL");
-            String username = System.getenv("DB_USERNAME");
-            String password = System.getenv("DB_PASSWORD");
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
 		//	conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
-		//	conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root","password");
-			conn = DriverManager.getConnection(url, username, password);
+			conn = DriverManager.getConnection("jdbc:mysql://16.170.225.12:3306/ecommerce","root","password");
 			
 		}catch (Exception e) {
 			e.printStackTrace();
